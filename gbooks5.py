f326460083e9b6ed12a88a1bcb160a2ad636f9ec
VishwaN22 <vpnanavati@gmail.com>
2023-10-11 12:05:24 +0530

    added more description files

--- gbooks5.py
+++ gbooks5.py
@@ -4,14 +4,14 @@ from googleapiclient.errors import HttpError
 import time
 import logging
 # log_file_path = "./logs/log_file5.txt"
-logging.basicConfig(filename="process_9_n.log", format='%(asctime)s - %(levelname)s - %(message)s', level=logging.DEBUG)
-books = build('books', 'v1', developerKey='AIzaSyAos12iQhJMoLfF6v1JgziSDuCPNl29Ucg')  
+logging.basicConfig(filename="process_10_n.log", format='%(asctime)s - %(levelname)s - %(message)s', level=logging.DEBUG)
+books = build('books', 'v1', developerKey=' ')  
 
-with open('.\jsons\part_9.json', 'r') as json_file:
+with open('.\jsons\part_10.json', 'r') as json_file:
     data = json.load(json_file)
 
 book_info = {}
-index = 7680
+index = 8640
 
 for entry in data:
     inner_dict = entry[str(index)]
@@ -45,7 +45,7 @@ for entry in data:
         logging.info(f'Data written to file for index: {index}')
     index += 1
     time.sleep(1)
-ofname='book_info_9_n.json'
+ofname='book_info_10_n.json'
 with open(ofname, 'w') as json_output:
     json.dump(book_info, json_output,indent=4)
     logging.info(f"Book info dictioanry dumped to a json: {ofname}")
